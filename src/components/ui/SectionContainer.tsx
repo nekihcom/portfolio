47790dd307bc3ff2b8869bf2855be58272d4e229
nekihcom <kmmch.tech@gmail.com>
2025-05-19 23:45:11 +0900

    コンテナのPC最大幅を960pxに変更した

--- src/components/ui/SectionContainer.tsx
+++ src/components/ui/SectionContainer.tsx
@@ -8,7 +8,7 @@ export const SectionContainer = (props:Props) => {
   const { id, className='', children } = props;
 
   return (
-    <section id={id} className={`w-[95%] sm:w-[90%] max-w-[400px] md:max-w-[780px] mx-auto py-16 ${className}`}>
+    <section id={id} className={`w-[95%] sm:w-[90%] max-w-[400px] md:max-w-[960px] mx-auto py-16 ${className}`}>
       {children}
     </section>
   );
