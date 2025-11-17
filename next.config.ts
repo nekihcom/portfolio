90a436c0b58418a168656dd69b4637504f9f0367
nekihcom <kmmch.tech@gmail.com>
2025-11-17 21:20:29 +0900

    fix: images.domainsをimages.remotePatternsに更新して非推奨警告を解消

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["qiita-user-contents.imgix.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "qiita-user-contents.imgix.net",
+      },
+    ],
   },
 };
 
