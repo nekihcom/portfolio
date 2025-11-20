e204775da80ab7e8d0fdb94c7bcc95a1f628906d
nekihcom <kmmch.tech@gmail.com>
2025-11-20 21:00:48 +0900

    feat: Vercel Analyticsを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
+
 import "./globals.css";
 import { ScrollToTop } from "@/components/common/ScrollToTop";
 import { Header } from "@/components/layout/Header";
@@ -36,6 +38,7 @@ export default function RootLayout({
         </div>
         <Footer />
         <ScrollToTop />
+        <Analytics />
       </body>
     </html>
   );
