47354953afe8e888cd218597da898f2184b93b9b
nekihcom <kmmch.tech@gmail.com>
2026-08-13 16:12:49 +0900

    feat: 問い合わせフォームの実URLに差し替え
    /contactのリダイレクト先を仮URLから実際のGoogle Form URLに変更

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
-// TODO: Google Formが作成でき次第、実際のフォームURLに差し替える
-const CONTACT_FORM_URL = "https://forms.google.com/";
+const CONTACT_FORM_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLSeh1ZkxbbvuNXcLy3Ql0EN0SxglVI0j_Zux2eywzaGmFPCn7A/viewform?usp=header";
 
 export default function ContactPage() {
   redirect(CONTACT_FORM_URL);
